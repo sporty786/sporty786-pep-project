18e8a0a12d329bf698082d92ab3f4cfbdd1a6f39
sporty786 <michelle.e.tracy@gmail.com>
2023-02-14 07:16:24 +0000

    Controller bare bones

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -2,6 +2,12 @@ package Controller;
 
 import io.javalin.Javalin;
 import io.javalin.http.Context;
+import Model.Account;
+import Model.Message;
+import Service.AccountService;
+import Service.MessageService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller. The endpoints you will need can be
@@ -9,6 +15,13 @@ import io.javalin.http.Context;
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
 public class SocialMediaController {
+    private AccountService accountService;
+    private MessageService messageService;
+
+    public SocialMediaController(){
+        this.accountService = new AccountService();
+        this.messageService = new MessageService();
+    }
     /**
      * In order for the test cases to work, you will need to write the endpoints in the startAPI() method, as the test
      * suite must receive a Javalin object from this method.
@@ -16,10 +29,36 @@ public class SocialMediaController {
      */
     public Javalin startAPI() {
         Javalin app = Javalin.create();
-        app.get("example-endpoint", this::exampleHandler);
+        app.post("/register", this::postUserRegistrationHandler);
+        app.post("/login", this::postLoginHandler);
+        app.post("/message", this::postCreateNewMessageHandler);
+        app.get("/messages", this::getAllMessagesHandler);
+        app.get("/messages/{message_id}", this::getOneMessageGivenMessageIdHandler);
+        app.delete("/messages/{message_id}", this::deleteMessageGivenMessageIdHandler);
+        app.patch("/messages/{message_id}", this::patchMessageGivenMessageIdHandler);
+        app.get("/accounts/{account_id}/messages", this::getAllMessagesFromUserGivenAccountIdHandler);
 
+        app.start(8080);
         return app;
     }
+
+    private void postUserRegistrationHandler(Context ctx) throws JsonProcessingException {}
+
+    private void postLoginHandler(Context ctx) throws JsonProcessingException {}
+
+    private void postCreateNewMessageHandler(Context ctx) throws JsonProcessingException {}
+
+    private void getAllMessagesHandler(Context ctx){}
+    
+    private void getOneMessageGivenMessageIdHandler(Context ctx){}
+
+    private void deleteMessageGivenMessageIdHandler(Context ctx){}
+
+    private void patchMessageGivenMessageIdHandler(Context ctx){}
+
+    private void getAllMessagesFromUserGivenAccountIdHandler(Context ctx){}
+
+
 
     /**
      * This is an example handler for an example endpoint.
